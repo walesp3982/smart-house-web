b4b1dfa87f690a2176f2df8f6b033dd4de54dbc8
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-10 20:46:43 -0400

    refactoring login route

---
+++ app/login/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState } from "react";
+import "./login.css";
+
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Iniciar Sesión</h1>
+
+        <form>
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            className="input"
+          />
+
+          <div className="password-container">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Contraseña"
+              className="input"
+            />
+
+            <span
+              className="eye"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <Visibility /> : <VisibilityOff />}
+            </span>
+          </div>
+
+          <button className="button">Ingresar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
