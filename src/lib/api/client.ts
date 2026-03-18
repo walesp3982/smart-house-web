a8a0e21cb417c19a5ad0a769b1d9a581ff480114
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-18 18:10:25 -0400

    add default env variable

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,5 +2,5 @@ import createClient from "openapi-fetch";
 import { paths } from "./types";
 
 export const apiClient = createClient<paths>({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  baseUrl: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000",
 });
