3c964d4f5c940957ab7eb8cd3ea691508878708f
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-03-18 20:35:18 -0400

    refactoring form and use react-hook-form

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -5,18 +5,50 @@ import PasswordField from "@/component/FormContainer/Field/Password";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { useForm, Controller } from "react-hook-form";
 
+type FormValues = {
+  email: string;
+  password: string;
+};
 export default function LoginForm() {
+  const { control, handleSubmit } = useForm<FormValues>({
+    defaultValues: { email: "", password: "" },
+  });
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
   return (
-    <FormContainer>
-      <Typography variant="h4" align="center">
-        Iniciar sesión
-      </Typography>
-      <TextField label="Email" fullWidth />
-      <PasswordField></PasswordField>
-      <Button variant="contained" size="large" fullWidth>
-        Entrar
-      </Button>
-    </FormContainer>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormContainer>
+        <Typography variant="h4" align="center">
+          Iniciar sesión
+        </Typography>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field, fieldState }) => (
+            <TextField
+              {...field}
+              label="Email"
+              error={!!fieldState.error}
+              helperText={fieldState.error?.message}
+            ></TextField>
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          render={({ field, fieldState }) => (
+            <PasswordField field={field} fieldState={fieldState} />
+          )}
+        />
+        <Button type="submit" variant="contained" size="large" fullWidth>
+          Entrar
+        </Button>
+      </FormContainer>
+    </form>
   );
 }
--- component/FormContainer/Field/Password.tsx
+++ component/FormContainer/Field/Password.tsx
@@ -4,14 +4,36 @@ import OutlinedInput from "@mui/material/OutlinedInput";
 import PasswordIcon from "../Icons/Password";
 import { useState } from "react";
 
-export default function PasswordField() {
+import {
+  ControllerFieldState,
+  ControllerRenderProps,
+  FieldPath,
+  FieldValues,
+} from "react-hook-form";
+
+interface PasswordFieldProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+> {
+  field: ControllerRenderProps<TFieldValues, TName>;
+  fieldState: ControllerFieldState;
+}
+export default function PasswordFieldProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>,
+>({ field, fieldState }: PasswordFieldProps<TFieldValues, TName>) {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () =>
     setShowPassword((show: boolean) => !show);
   return (
     <FormControl variant="outlined">
-      <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+      <InputLabel
+        htmlFor="outlined-adornment-password"
+        error={!!fieldState.error}
+      >
+        Password
+      </InputLabel>
       <OutlinedInput
         id="outlined-adornment-password"
         type={showPassword ? "text" : "password"}
@@ -22,6 +44,7 @@ export default function PasswordField() {
           ></PasswordIcon>
         }
         label="Password"
+        {...field}
       />
     </FormControl>
   );
