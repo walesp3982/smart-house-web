ee0844a38b2804acd26dce64bb419ce82241e2b4
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-04-07 17:57:12 -0400

    cambio de nombre de archivo

--- component/Form/FormTextField/index.ts
+++ component/Form/FormTextField/index.ts
@@ -1,3 +1,3 @@
 export { default as BasicFieldText } from "./BasicTextField"
-export { default as PasswordFieldText } from "./PasswordFieldText"
+export { default as PasswordFieldText } from "./PasswordTextField"
 export { default as EmailTextField } from "./EmailTextField"
