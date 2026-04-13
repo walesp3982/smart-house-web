94116a44ac5edd4d35899925b3fba024cf3fa1e6
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-04-13 16:09:59 -0400

    format dialog

--- app/dashboard/devices/CreateNewHouseDialog.tsx
+++ app/dashboard/devices/CreateNewHouseDialog.tsx
@@ -4,48 +4,48 @@ import { useNewHouse } from "@/hooks/useNewHouse";
 import { Button, Stack } from "@mui/material";
 
 interface CreateNewHouseDialogProps {
-    activatedDialog: boolean
-    desactivatedDialog: () => void
+  activatedDialog: boolean;
+  desactivatedDialog: () => void;
 }
 
 export function CreateNewHouseDialog({
-    activatedDialog, desactivatedDialog
+  activatedDialog,
+  desactivatedDialog,
 }: CreateNewHouseDialogProps) {
-    const { form, isLoading, onSubmit } = useNewHouse()
-    const { control, handleSubmit } = form;
+  const { form, isLoading, onSubmit } = useNewHouse();
+  const { control, handleSubmit } = form;
 
-    return (
-        <AppDialog
-            open={activatedDialog}
-            onClose={desactivatedDialog}
-            title="Agregar nueva casa"
-            description="ingrese los datos para crear una nueva casa"
-            onSubmit={handleSubmit(onSubmit)}
-            actions={
-                <>
-                    <Button onClick={desactivatedDialog} disabled={isLoading}>
-                        Cancelar
-                    </Button>
-                    <Button
-                        type="submit" variant="contained" disabled={isLoading}>
-                        {isLoading ? "Cargando..." : "Enviar"}
-                    </Button>
-                </>
-            }
-        >
-            <Stack spacing={2} sx={{ my: 4 }}>
-                <BasicTextField
-                    control={control}
-                    name="name"
-                    label="Nombre de la casa"
-                />
+  return (
+    <AppDialog
+      open={activatedDialog}
+      onClose={desactivatedDialog}
+      title="Agregar nueva casa"
+      description="ingrese los datos para crear una nueva casa"
+      onSubmit={handleSubmit(onSubmit)}
+      actions={
+        <>
+          <Button onClick={desactivatedDialog} disabled={isLoading}>
+            Cancelar
+          </Button>
+          <Button type="submit" variant="contained" disabled={isLoading}>
+            {isLoading ? "Cargando..." : "Enviar"}
+          </Button>
+        </>
+      }
+    >
+      <Stack spacing={2} sx={{ my: 4 }}>
+        <BasicTextField
+          control={control}
+          name="name"
+          label="Nombre de la casa"
+        />
 
-                <BasicTextField
-                    control={control}
-                    name="location"
-                    label="Ubicación (Opcional)"
-                />
-            </Stack>
-        </AppDialog>
-    )
+        <BasicTextField
+          control={control}
+          name="location"
+          label="Ubicación (Opcional)"
+        />
+      </Stack>
+    </AppDialog>
+  );
 }
