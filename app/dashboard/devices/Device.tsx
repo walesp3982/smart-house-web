1fe35d5f39237a94e904dcf80396963ec568be8d
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-04-23 20:11:31 -0400

    finish state device

--- app/dashboard/devices/Device.tsx
+++ app/dashboard/devices/Device.tsx
@@ -1,14 +1,27 @@
 import { DeviceItem, StatusColor } from "@/component/Devices/Item";
-import { DeviceStatus, InstalledDeviceType, selectDeviceState, useInstalledDevicesStore } from "@/store/installed-devices-store";
+import {
+  // DeviceStatus,
+  InstalledDeviceType,
+  selectDeviceState,
+  useInstalledDevicesStore,
+} from "@/store/installed-devices-store";
 import { useStateDevice } from "@/hooks/useStateDevice";
-import { Box, Grid, Paper } from "@mui/material";
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DoorFrontIcon from '@mui/icons-material/DoorFront';
-import MotionPhotosOnIcon from '@mui/icons-material/MotionPhotosAuto';
-import ThermostatIcon from '@mui/icons-material/Thermostat';
-import VideocamIcon from '@mui/icons-material/Videocam';
-import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
-import { SvgIconProps } from '@mui/material';
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DoorFrontIcon from "@mui/icons-material/DoorFront";
+import MotionPhotosOnIcon from "@mui/icons-material/MotionPhotosAuto";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
+import { SvgIconProps } from "@mui/material";
 
 const deviceIconMap: Record<string, React.ElementType> = {
   light: LightModeIcon,
@@ -18,8 +31,10 @@ const deviceIconMap: Record<string, React.ElementType> = {
   camera: VideocamIcon,
 };
 
-
-export function getDeviceIcon(type: string, props?: SvgIconProps): React.ReactElement {
+export function getDeviceIcon(
+  type: string,
+  props?: SvgIconProps,
+): React.ReactElement {
   const IconComponent = deviceIconMap[type.toLowerCase()] ?? DevicesOtherIcon;
   return <IconComponent {...props} />;
 }
@@ -28,7 +43,6 @@ interface DeviceContainerProps {
   devices: InstalledDeviceType[]; // tu tipo del store
   onEdit: (device: InstalledDeviceType) => void;
 }
-
 
 export function DeviceContainer({ devices, onEdit }: DeviceContainerProps) {
   return (
@@ -55,34 +69,15 @@ export function DeviceItemActivity({
   indentLevel = 0,
   onEdit,
 }: DeviceItemProps) {
-  useStateDevice(device.id)
+  useStateDevice(device.id);
 
   const { status, lastMessage } = useInstalledDevicesStore(
-    selectDeviceState(device.id)
-  )
+    selectDeviceState(device.id),
+  );
 
-  const color_status: StatusColor = status === "open" ? "green" : status === "error" ? "red" : "yellow"
-  const text_status = (status: DeviceStatus): string => {
-    switch (status) {
-      case "idle":
-        return "Intentando conectarse..."
-      case "fetching-ticket":
-        return "Obteniendo token..."
-      case "connecting":
-        return "Conectando..."
-      case "open":
-        return "Conexión exitosa"
-      case "error":
-        return "Error en la conexión"
-      case "closed":
-        return "Conexión cerrada"
-    }
-  }
-  const json_message = JSON.parse(lastMessage ?? '{"message": "json no encontrado"}');
-  // const device_icon = (
-  //   <DevicesIcon fontSize="small" sx={{ color: "text.secondary" }} />
-  // );
-  const device_icon = getDeviceIcon(device.device.type)
+  const color_status: StatusColor =
+    status === "open" ? "green" : status === "error" ? "red" : "yellow";
+  const device_icon = getDeviceIcon(device.device.type);
   return (
     <>
       <DeviceItem
@@ -93,22 +88,75 @@ export function DeviceItemActivity({
         device_icon={device_icon}
         color={color_status}
       />
-      <Box display={"block"}>
-        <Paper sx={{
+      {/* <Paper sx={{
           padding: 1,
         }}>
           {text_status(status)}
-        </Paper>
-        <Paper sx={{
+        </Paper> */}
+      <Paper
+        sx={{
           padding: 1,
-        }}>
-          {json_message.message}
-        </Paper>
-
-
-      </Box>
-
+          gap: 5,
+          py: 2,
+        }}
+      >
+        <Stack spacing={2}>
+          <KeyValueDisplay data={JSON.parse(lastMessage ?? "{}")} />
+          <Button fullWidth variant="contained">
+            Encender
+          </Button>
+        </Stack>
+      </Paper>
     </>
+  );
+}
 
+const EXCLUDED_KEYS = ["type", "ip", "message", "status"];
+interface StateDeviceDisplay {
+  data: Record<string, string | number>;
+  exclude?: string[];
+}
+
+function KeyValueDisplay({
+  data,
+  exclude = EXCLUDED_KEYS,
+}: StateDeviceDisplay) {
+  // Exclusión de valores que no queremos mostrar al user
+  const entries = Object.entries(data).filter(
+    ([key]) => !exclude?.includes(key),
+  );
+
+  return (
+    <Box
+      sx={{
+        border: "1px solid",
+        borderColor: "divider",
+        borderRadius: 2,
+        overflow: "hidden",
+      }}
+    >
+      {entries.map(([key, value], i) => (
+        <Box key={key}>
+          <Box
+            sx={{
+              display: "flex",
+              gap: 2,
+              px: 2,
+              py: 1.25,
+              alignItems: "baseline",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" color="text.secondary">
+              {key}:
+            </Typography>
+            <Typography variant="body2" fontWeight={500}>
+              {String(value)}
+            </Typography>
+          </Box>
+          {i < entries.length - 1 && <Divider />}
+        </Box>
+      ))}
+    </Box>
   );
 }
