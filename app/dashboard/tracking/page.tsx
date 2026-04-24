a8f35bd7cd7b5b29999e4e6127fc7ed1765a0a11
Walter Espejo <josuewalterespejoguarachi@gmail.com>
2026-04-23 21:30:15 -0400

    track_device finished hover

--- app/dashboard/tracking/page.tsx
+++ app/dashboard/tracking/page.tsx
@@ -9,6 +9,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import styles from "../Dashboard.module.css";
@@ -16,6 +17,11 @@ import { translations } from "../tranlations";
 import { getTrackData } from "@/actions/track.actions";
 import { useEffect, useState } from "react";
 import { TrackDeviceType } from "@/lib/api/services";
+import {
+  InstalledDeviceType,
+  useInstalledDevicesStore,
+} from "@/store/installed-devices-store";
+import { useHouseStore } from "@/store/house-store";
 
 const t = (key: keyof typeof translations) => translations[key];
 
@@ -31,14 +37,53 @@ function formatDate(dateString: string) {
   });
 }
 
+interface MetadataDeviceProps {
+  device?: InstalledDeviceType;
+}
+function MetadataDevice({ device }: MetadataDeviceProps) {
+  const houses = useHouseStore((state) => state.house);
+  const actual_house = houses?.find((h) => h.id == device?.house_id);
+  const actual_area = actual_house?.areas.find((a) => a.id == device?.id);
+  return (
+    <Tooltip
+      title={
+        <div style={{ padding: 4, fontSize: "1rem" }}>
+          <div>
+            <b>Tipo:</b> {device?.device.type}
+          </div>
+          <div>
+            <b>UUID: </b> {device?.device.device_uuid}
+          </div>
+          <div>
+            <b>Casa:</b> {actual_house?.name ?? "(Ninguna)"}
+          </div>
+          <div>
+            <b>Area: </b> {actual_area?.name ?? "(Ninguna)"}
+          </div>
+        </div>
+      }
+    >
+      <span style={{ cursor: "pointer" }}>{device?.name}</span>
+    </Tooltip>
+  );
+}
+
 export default function TrackingPage() {
   const [data, setData] = useState<TrackDeviceType[] | null>(null);
-
+  const installed_devices = useInstalledDevicesStore(
+    (state) => state.installedDevices,
+  );
   useEffect(() => {
     let mounted = true;
 
     if (mounted) {
-      getTrackData().then((d) => setData(d));
+      getTrackData().then((d) => {
+        d?.sort(
+          (a, b) =>
+            new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime(),
+        );
+        setData(d);
+      });
     }
 
     return () => {
@@ -74,7 +119,13 @@ export default function TrackingPage() {
               {data.map((row) => (
                 <TableRow key={row.id}>
                   <TableCell>{formatDate(row.timestamp)}</TableCell>
-                  <TableCell>{row.device_id}</TableCell>
+                  <TableCell>
+                    <MetadataDevice
+                      device={installed_devices?.find(
+                        (d) => d.id == row.device_id,
+                      )}
+                    />
+                  </TableCell>
                   <TableCell>{row.status}</TableCell>
                 </TableRow>
               ))}
